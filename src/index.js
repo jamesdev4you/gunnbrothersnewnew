4dea091b7c60de163c931b7241bf5d4b76ac6654
James <114611738+jamesdev4you@users.noreply.github.com>
2023-05-19 21:00:45 -0400

    final4

--- src/index.js
+++ src/index.js
@@ -7,10 +7,8 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  
     <BrowserRouter>
       <ScrollToTop />
       <App />
     </BrowserRouter>
-  
 );
